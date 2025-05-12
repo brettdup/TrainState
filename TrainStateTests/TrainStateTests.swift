29dc3948f1400088685445e73e6fa9b02a6a1d0a
Brett <brett@scnet.co.za>
2025-05-12 08:03:15 +0200

    Initial Commit

---
+++ TrainStateTests/TrainStateTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrainStateTests.swift
+//  TrainStateTests
+//
+//  Created by Brett du Plessis on 2025/05/12.
+//
+
+import Testing
+@testable import TrainState
+
+struct TrainStateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
