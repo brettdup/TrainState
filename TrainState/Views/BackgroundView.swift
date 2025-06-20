1907763900c4c5e720a6145c7d51672f4f14cff5
Brett <duplessisbrett@icloud.com>
2025-06-20 15:19:41 +0200

    update to background colour

--- TrainState/Views/BackgroundView.swift
+++ TrainState/Views/BackgroundView.swift
@@ -1,27 +1,27 @@
 import SwiftUI
 
 struct BackgroundView: View {
-    // Customizable properties
-    var gradientColors: [Color] = [
-        Color(red: 0.7, green: 1.0, blue: 0.7),     // Soft green
-        Color(red: 0.7, green: 0.85, blue: 1.0),    // Soft blue
-    ]
-    var opacity: Double = 0.9
-    
-    var body: some View {
-        LinearGradient(
-            gradient: Gradient(colors: gradientColors),
-            startPoint: .topLeading,
-            endPoint: .bottomTrailing
-        )
-        .opacity(opacity)
-        .ignoresSafeArea()
-    }
+  // Customizable properties
+  var gradientColors: [Color] = [
+    Color(red: 0.7, green: 1.0, blue: 0.78),  // Soft green
+    Color(red: 0.7, green: 0.85, blue: 1.0),  // Soft blue
+  ]
+  var opacity: Double = 0.9
+
+  var body: some View {
+    LinearGradient(
+      gradient: Gradient(colors: gradientColors),
+      startPoint: .topLeading,
+      endPoint: .bottomTrailing
+    )
+    .opacity(opacity)
+    .ignoresSafeArea()
+  }
 }
 
 // Preview provider for SwiftUI canvas
 struct BackgroundView_Previews: PreviewProvider {
-    static var previews: some View {
-        BackgroundView()
-    }
-} 
+  static var previews: some View {
+    BackgroundView()
+  }
+}
